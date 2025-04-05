c4319a22dea7a2a9e993de28f37617556340fe74
redth <jondick@gmail.com>
2025-04-05 13:51:45 -0400

    More logging, and use emulator directly
    This gets the logging captured directly by supervisord

--- scripts/emulator.sh
+++ scripts/emulator.sh
@@ -1,4 +1,6 @@
-#!/usr/bin/env bash
+#!/bin/bash
+
+export ANDROID_TOOL_PROCESS_RUNNER_LOG_PATH=/logs/androidsdktool.log
 
 # Permissions for KVM
 sudo chown 1400:1401 /dev/kvm
@@ -8,4 +10,5 @@ sudo chown 1400:1401 /dev/kvm
 /home/mauiusr/.android/platform-tools/adb start-server
 
 # Start our emulator
-android avd start --home="${ANDROID_HOME}" --name=Emulator_${ANDROID_SDK_API_LEVEL} --wait-boot --wait-exit --gpu="swiftshader_indirect" --accel="on" --wipe-data --no-window --no-audio --no-boot-anim --grpc=8554
+#android avd start --home="${ANDROID_HOME}" --name=Emulator_${ANDROID_SDK_API_LEVEL} --wait-boot --wait-exit --gpu="swiftshader_indirect" --accel="on" --wipe-data --no-window --no-audio --no-boot-anim --grpc=8554
+/home/mauiusr/.android/emulator/emulator -avd Emulator_${ANDROID_SDK_API_LEVEL} -no-snapshot-load -grpc 8554 -wipe-data -gpu swiftshader_indirect -accel on -no-window -no-audio -no-boot-anim
