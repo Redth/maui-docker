a92942a21acd0fa89d21afdd6d40e99fa191607e
redth <jondick@gmail.com>
2025-04-05 13:51:07 -0400

    Make both socat processes run

--- scripts/androidportforward.sh
+++ scripts/androidportforward.sh
@@ -1,8 +1,8 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 # Get the local IP address
 local_ip=$(hostname -I | awk '{print $1}')
 
 # Forward the ports for adb/emulator so we can connect from the host
-/usr/bin/socat tcp-listen:5554,bind=${local_ip},fork tcp:127.0.0.1:5554
+/usr/bin/socat tcp-listen:5554,bind=${local_ip},fork tcp:127.0.0.1:5554 &
 /usr/bin/socat tcp-listen:5555,bind=${local_ip},fork tcp:127.0.0.1:5555
