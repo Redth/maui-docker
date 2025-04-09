f17d52aecba9c2f08359936af99dcfc91e8d053a
redth <jondick@gmail.com>
2025-04-08 21:49:53 -0400

    More run options

--- run.ps1
+++ run.ps1
@@ -1,8 +1,10 @@
 Param([String]$AndroidSdkApiLevel=35,
+    [String]$AdbKeyFolder="$env:USERPROFILE/.android",
     [Int]$AdbPortMapping=5555,
     [Int]$EmulatorPortMapping=5554,
     [Int]$GrpcPortMapping=8554,
-    [Int]$AppiumPortMapping=4723)
+    [Int]$AppiumPortMapping=4723,
+    [bool]$BindAdbKeys=$true)
 
 
 $runArgs = @(
@@ -11,8 +13,15 @@ $runArgs = @(
     "-p", "${AdbPortMapping}:5555/tcp",
     "-p", "${EmulatorPortMapping}:5554/tcp",
     "-p", "${GrpcPortMapping}:8554/tcp",
-    "-p", "${AppiumPortMapping}:4723/tcp",
-    "redth/maui-docker:android_appium_emulator_$AndroidSdkApiLevel"
+    "-p", "${AppiumPortMapping}:4723/tcp"
 )
+
+if ($bindAdbKeys) {
+    $runArgs += "--mount", "type=bind,src=${AdbKeyFolder}/adbkey,dst=/home/mauiusr/.android/adbkey,readonly"
+    $runArgs += "--mount", "type=bind,src=${AdbKeyFolder}/adbkey.pub,dst=/home/mauiusr/.android/adbkey.pub,readonly"
+}
+
+
+$runArgs += "redth/maui-docker:android_appium_emulator_$AndroidSdkApiLevel"
 
 & docker $runArgs
